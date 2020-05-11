018ffd31b337a2b6a13d3c56a05498b15ebb1bca
Siyana <siyana.stoyanov@gmail.com>
2020-05-11 21:12:45 +0200

    Rearranged both forms and added components to statustics form

--- Code/FireSimulation/Statistics.Designer.cs
+++ Code/FireSimulation/Statistics.Designer.cs
@@ -29,162 +29,202 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnReplay = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pb_search = new System.Windows.Forms.PictureBox();
+            this.tb_search_input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnReplay = new System.Windows.Forms.Button();
+            this.pb_layout = new System.Windows.Forms.PictureBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.lb_search_results = new System.Windows.Forms.ListBox();
+            this.cb_filter = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
-            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_search)).BeginInit();
             this.groupBox5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_layout)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(562, 12);
+            this.groupBox1.Location = new System.Drawing.Point(633, 56);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(367, 489);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(268, 427);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.btnReplay);
-            this.groupBox2.Location = new System.Drawing.Point(973, 12);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(367, 207);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.pictureBox2);
-            this.groupBox3.Controls.Add(this.textBox1);
-            this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Location = new System.Drawing.Point(973, 232);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(367, 489);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.label3);
-            this.groupBox4.Location = new System.Drawing.Point(12, 525);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(745, 196);
-            this.groupBox4.TabIndex = 2;
-            this.groupBox4.TabStop = false;
-            // 
-            // groupBox5
-            // 
-            this.groupBox5.Controls.Add(this.label4);
-            this.groupBox5.Location = new System.Drawing.Point(795, 525);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(134, 196);
-            this.groupBox5.TabIndex = 2;
-            this.groupBox5.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(135, 18);
+            this.label1.Location = new System.Drawing.Point(81, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 29);
+            this.label1.Size = new System.Drawing.Size(80, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Statistics";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnReplay);
+            this.groupBox2.Location = new System.Drawing.Point(937, 37);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(275, 168);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnReplay
+            // 
+            this.btnReplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReplay.Location = new System.Drawing.Point(20, 58);
+            this.btnReplay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnReplay.Name = "btnReplay";
+            this.btnReplay.Size = new System.Drawing.Size(238, 57);
+            this.btnReplay.TabIndex = 0;
+            this.btnReplay.Text = "Reply selected simulation";
+            this.btnReplay.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lb_search_results);
+            this.groupBox3.Controls.Add(this.pb_search);
+            this.groupBox3.Controls.Add(this.tb_search_input);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(937, 238);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(275, 421);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // pb_search
+            // 
+            this.pb_search.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb_search.Image = global::FireSimulator.Icons.search_Icon;
+            this.pb_search.ImageLocation = "";
+            this.pb_search.Location = new System.Drawing.Point(225, 58);
+            this.pb_search.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pb_search.Name = "pb_search";
+            this.pb_search.Size = new System.Drawing.Size(17, 18);
+            this.pb_search.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_search.TabIndex = 4;
+            this.pb_search.TabStop = false;
+            // 
+            // tb_search_input
+            // 
+            this.tb_search_input.Location = new System.Drawing.Point(37, 58);
+            this.tb_search_input.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_search_input.Name = "tb_search_input";
+            this.tb_search_input.Size = new System.Drawing.Size(184, 20);
+            this.tb_search_input.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(122, 18);
+            this.label2.Location = new System.Drawing.Point(99, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 29);
+            this.label2.Size = new System.Drawing.Size(70, 24);
             this.label2.TabIndex = 2;
             this.label2.Text = "Search";
             // 
-            // label3
+            // groupBox4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(313, 18);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Selector";
+            this.groupBox4.Location = new System.Drawing.Point(35, 521);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox4.Size = new System.Drawing.Size(666, 138);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.cb_filter);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Location = new System.Drawing.Point(728, 521);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox5.Size = new System.Drawing.Size(173, 138);
+            this.groupBox5.TabIndex = 2;
+            this.groupBox5.TabStop = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(29, 18);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 40);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.Size = new System.Drawing.Size(51, 24);
             this.label4.TabIndex = 4;
-            this.label4.Text = "Sort By";
+            this.label4.Text = "Filter";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // textBox1
+            // pb_layout
             // 
-            this.textBox1.Location = new System.Drawing.Point(49, 71);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(227, 22);
-            this.textBox1.TabIndex = 3;
+            this.pb_layout.Location = new System.Drawing.Point(35, 28);
+            this.pb_layout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pb_layout.Name = "pb_layout";
+            this.pb_layout.Size = new System.Drawing.Size(565, 473);
+            this.pb_layout.TabIndex = 0;
+            this.pb_layout.TabStop = false;
             // 
-            // pictureBox2
+            // hScrollBar1
             // 
-            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox2.Image = global::FireSimulator.Icons.search_Icon;
-            this.pictureBox2.ImageLocation = "";
-            this.pictureBox2.Location = new System.Drawing.Point(282, 71);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(23, 22);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
+            this.hScrollBar1.Location = new System.Drawing.Point(37, 659);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(662, 15);
+            this.hScrollBar1.TabIndex = 4;
             // 
-            // pictureBox1
+            // lb_search_results
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(500, 489);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.lb_search_results.FormattingEnabled = true;
+            this.lb_search_results.Location = new System.Drawing.Point(37, 92);
+            this.lb_search_results.Name = "lb_search_results";
+            this.lb_search_results.Size = new System.Drawing.Size(205, 303);
+            this.lb_search_results.TabIndex = 5;
             // 
-            // btnReplay
+            // cb_filter
             // 
-            this.btnReplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReplay.Location = new System.Drawing.Point(31, 59);
-            this.btnReplay.Name = "btnReplay";
-            this.btnReplay.Size = new System.Drawing.Size(317, 70);
-            this.btnReplay.TabIndex = 0;
-            this.btnReplay.Text = "Reply selected simulation";
-            this.btnReplay.UseVisualStyleBackColor = true;
+            this.cb_filter.FormattingEnabled = true;
+            this.cb_filter.Items.AddRange(new object[] {
+            "Most Deaths",
+            "Least Deaths",
+            "Most Successsful",
+            "Least Successful"});
+            this.cb_filter.Location = new System.Drawing.Point(18, 80);
+            this.cb_filter.Name = "cb_filter";
+            this.cb_filter.Size = new System.Drawing.Size(137, 21);
+            this.cb_filter.TabIndex = 5;
             // 
             // Statistics
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1358, 733);
+            this.ClientSize = new System.Drawing.Size(1258, 704);
+            this.Controls.Add(this.hScrollBar1);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pb_layout);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Statistics";
             this.Text = "Statistics";
             this.groupBox1.ResumeLayout(false);
@@ -192,30 +232,30 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
-            this.groupBox4.ResumeLayout(false);
-            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_search)).EndInit();
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_layout)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pb_layout;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pb_search;
+        private System.Windows.Forms.TextBox tb_search_input;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnReplay;
+        private System.Windows.Forms.ListBox lb_search_results;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.ComboBox cb_filter;
     }
 }
