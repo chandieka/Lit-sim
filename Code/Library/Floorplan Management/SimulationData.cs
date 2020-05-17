dd9a917ca4878ccc25215cdf3177100969f65ab1
chandieka <candiekatasbihyantra2@gmail.com>
2020-05-17 12:04:56 +0200

    add nrOfSurviver Property

--- Code/Library/Floorplan Management/SimulationData.cs
+++ Code/Library/Floorplan Management/SimulationData.cs
@@ -9,6 +9,7 @@ namespace Library
 		public readonly TimeSpan SimulationTime;
 		public readonly int NrOfPeople;
 		public readonly int NrOfDeaths;
+        public readonly int NrOfSurviver;
 
 		public SimulationData(int nrOfDeaths, int nrOfPeople, DateTime date, TimeSpan time)
 		{
@@ -16,6 +17,7 @@ namespace Library
 			this.NrOfDeaths = nrOfDeaths;
 			this.DateOfSimulation = date;
 			this.SimulationTime = time;
+            NrOfSurviver = NrOfPeople - nrOfDeaths;
 		}
 	}
 }
