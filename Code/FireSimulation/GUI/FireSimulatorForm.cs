0bce3195402efad9bcbd8427ea45692277dfd112
Schroijen,Jelle J.C.B <j.schroijen@student.fontys.nl>
2020-05-18 23:22:17 +0200

    Revert "Revert "Add the possibility to alter the time limit""
    This reverts commit 60e0ef384a873317b7712de426bdecd45b681684.

--- Code/FireSimulation/GUI/FireSimulatorForm.cs
+++ Code/FireSimulation/GUI/FireSimulatorForm.cs
@@ -1,239 +1,256 @@
-﻿using Library;
+﻿using FireSimulator.GUI;
+using Library;
 using System;
 using System.Windows.Forms;
 
 namespace FireSimulator
 {
-	public partial class FireSimulatorForm : Form
-	{
-		private TimeSpan time = new TimeSpan(0, 0, 0);
-		private Simulator simStartingPoint;
-		private Simulator simulator;
-		private TimeSpan timeLimit;
+    public partial class FireSimulatorForm : Form
+    {
+        private TimeSpan time = new TimeSpan(0, 0, 0);
+        private Simulator simStartingPoint;
+        private Simulator simulator;
+        private TimeSpan timeLimit;
 
-		private readonly SaveItem saveItem;
-		private bool running;
-		private bool hasRun;
+        private readonly SaveItem saveItem;
+        private bool running;
+        private bool hasRun;
 
-		public FireSimulatorForm(SaveItem saveItem)
-		{
-			InitializeComponent();
+        public FireSimulatorForm(SaveItem saveItem)
+        {
+            InitializeComponent();
 
-			WindowState = FormWindowState.Maximized;
-			lblElapsedTime.Text = time.ToString();
+            WindowState = FormWindowState.Maximized;
+            lblElapsedTime.Text = time.ToString();
 
-			this.Text = "Lit - Simulator";
+            this.Text = "Lit - Simulator";
 
-			if (!(saveItem.Item is Layout))
-				throw new Exception("Cannot simulate SaveItem that does not contain a Layout");
+            if (!(saveItem.Item is Layout))
+                throw new Exception("Cannot simulate SaveItem that does not contain a Layout");
 
-			this.saveItem = saveItem;
-			this.simulator = new Simulator((Layout)saveItem.Item);
-			this.simStartingPoint = simulator.DeepCloneSelf();
-			this.simulator.Finished += this.handleFinished;
+            this.saveItem = saveItem;
+            this.simulator = new Simulator((Layout)saveItem.Item);
+            this.simStartingPoint = simulator.DeepCloneSelf();
+            this.simulator.Finished += this.handleFinished;
 
-			// Static Data
-			lblFireExTotal.Text = simulator.FireExtinguisherAmount.ToString();
-			lblPeopleTotal.Text = simulator.PersonAmount.ToString();
-			lblDate.Text = DateTime.Today.ToString("dd/MM/yyyy");
+            // Static Data
+            lblFireExTotal.Text = simulator.FireExtinguisherAmount.ToString();
+            lblPeopleTotal.Text = simulator.PersonAmount.ToString();
+            lblDate.Text = DateTime.Today.ToString("dd/MM/yyyy");
 
-			VisualizeSimulation();
-		}
+            // Set the default time limit
+            SetTimeLimit(0, 10, 0);
 
-		#region Private Methods
-		private void handleFinished(object sender, FinishedEventArgs e)
-		{
-			picBoxPlayPause_Click(null, null);
-			btnCalculatePaths.Enabled = false;
+            VisualizeSimulation();
+        }
 
-			this.simulator.ClearHistory();
-			this.UpdateHistory();
-			this.simulator.AddToHistory("Simulation finished");
+        #region Private Methods
 
-			btnRerunSimulation.Visible = true;
-			picBoxPlayPause.Enabled = false;
+        private void handleFinished(object sender, FinishedEventArgs e)
+        {
+            picBoxPlayPause_Click(null, null);
+            btnCalculatePaths.Enabled = false;
 
-			if (e.IsSuccess)
-				lblResult.Text = "Success";
-			else
-				lblResult.Text = "Fail";
+            this.simulator.ClearHistory();
+            this.UpdateHistory();
+            this.simulator.AddToHistory("Simulation finished");
 
-			if (MessageBox.Show($"The simulation finished \"{e.ToString()}\"\nDo you want to save the simulation data?",
-				"Finish", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
-				simulator.SaveSimulationData(saveItem, DateTime.Now, time);
-		}
+            btnRerunSimulation.Visible = true;
+            picBoxPlayPause.Enabled = false;
 
-		private void SetTimeLimit(int hours = 0, int minutes = 0, int seconds = 0)
-		{
-			timeLimit = new TimeSpan(hours, minutes, seconds);
-		}
+            if (e.IsSuccess)
+                lblResult.Text = "Success";
+            else
+                lblResult.Text = "Fail";
 
-		private void LoadStartingLayout()
-		{
-			if (simStartingPoint != null)
-			{
-				simulator.Finished -= handleFinished;
-				simulator = simStartingPoint.DeepCloneSelf();
-				simulator.Finished += handleFinished;
-			}
-		}
+            if (MessageBox.Show($"The simulation finished \"{e.ToString()}\"\nDo you want to save the simulation data?",
+                "Finish", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                simulator.SaveSimulationData(saveItem, DateTime.Now, time);
+        }
 
-		public void ReStart()
-		{
-			hasRun = false;
-			running = false;
-			time = TimeSpan.Zero;
-			btnRerunSimulation.Visible = false;
-			lblResult.Text = "<Success/Fail>";
-			trackBarSpeed.Value = 50;
-			animationLoopTimer.Interval = 60;
+        public void SetTimeLimit(int hours = 0, int minutes = 0, int seconds = 0)
+        {
+            timeLimit = new TimeSpan(hours, minutes, seconds);
+        }
 
-			// TODO: Paths shouldn't have to be recalculated...
+        public TimeSpan GetTimeLimit()
+        {
+            return this.timeLimit;
+        }
 
-			GetStats();
-			LoadStartingLayout();
-			VisualizeSimulation();
-		}
+        private void LoadStartingLayout()
+        {
+            if (simStartingPoint != null)
+            {
+                simulator.Finished -= handleFinished;
+                simulator = simStartingPoint.DeepCloneSelf();
+                simulator.Finished += handleFinished;
+            }
+        }
 
-		private void UpdateHistory()
-		{
-			this.lbHistory.Items.Clear();
-			this.lbHistory.Items.AddRange(this.simulator.GetHistory());
-		}
+        public void ReStart()
+        {
+            hasRun = false;
+            running = false;
+            time = TimeSpan.Zero;
+            btnRerunSimulation.Visible = false;
+            lblResult.Text = "<Success/Fail>";
+            trackBarSpeed.Value = 50;
+            animationLoopTimer.Interval = 60;
 
-		private void GetStats()
-		{
-			// Dynamic Data
-			var deathAmount = simulator.GetNrOfDeaths();
+            // TODO: Paths shouldn't have to be recalculated...
 
-			lblAlive.Text = (simulator.PersonAmount - deathAmount).ToString();
-			lblDeaths.Text = deathAmount.ToString();
-			lblElapsedTime.Text = time.ToString();
-		}
+            GetStats();
+            LoadStartingLayout();
+            VisualizeSimulation();
+        }
 
-		private void VisualizeSimulation()
-		{
-			pbSimulator.Image = simulator.Paint((6, 6), !hasRun); // TODO: Check these numbers
-		}
-		#endregion Private Methods
+        private void UpdateHistory()
+        {
+            this.lbHistory.Items.Clear();
+            this.lbHistory.Items.AddRange(this.simulator.GetHistory());
+        }
 
-		#region EventHandlers
-		private void animationLoopTimer_Tick(object sender, EventArgs e)
-		{
-			TimeSpan second = new TimeSpan(0, 0, 1);
-			time = time.Add(second);
+        private void GetStats()
+        {
+            // Dynamic Data
+            var deathAmount = simulator.GetNrOfDeaths();
 
-			simulator.Tick();
-			GetStats();
-			VisualizeSimulation();
+            lblAlive.Text = (simulator.PersonAmount - deathAmount).ToString();
+            lblDeaths.Text = deathAmount.ToString();
+            lblElapsedTime.Text = time.ToString();
+        }
 
-			// TODO: User should be able to allow the limit
-			if (time.Ticks == timeLimit.Ticks)
-				simulator.TimeLimitReached();
-		}
+        private void VisualizeSimulation()
+        {
+            pbSimulator.Image = simulator.Paint((6, 6), !hasRun); // TODO: Check these numbers
+        }
 
-		private void picBoxPlayPause_Click(object sender, EventArgs e)
-		{
-			if (!picBoxPlayPause.Enabled)
-				return;
+        #endregion Private Methods
 
-			if (!running)
-			{
-				hasRun = true;
+        #region EventHandlers
 
-				try
-				{
-					animationLoopTimer.Start();
-					running = true;
-					picBoxPlayPause.Image = Icons.Pause;
-					toolTipPlay.SetToolTip(picBoxPlayPause, "Pause (Spacebar)");
-					this.simulator.AddToHistory("Simulation started");
-					UpdateHistory();
-					this.lbHistory.Enabled = false;
-					btnTerminate.Visible = false;
+        private void animationLoopTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan second = new TimeSpan(0, 0, 1);
+            time = time.Add(second);
 
-					// TODO: Make the time limit change dynamically
-					SetTimeLimit(0, 10, 0);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.Message);
-				}
-			}
-			else
-			{
-				animationLoopTimer.Stop();
-				running = false;
-				picBoxPlayPause.Image = Icons.Play;
-				toolTipPlay.SetToolTip(picBoxPlayPause, "Resume (Spacebar)");
-				this.simulator.AddToHistory("Simulation paused");
-				UpdateHistory();
-				this.lbHistory.Enabled = true;
-				btnTerminate.Visible = true;
-			}
-		}
+            simulator.Tick();
+            GetStats();
+            VisualizeSimulation();
 
-		private void btnCloseStatistics_Click(object sender, EventArgs e)
-		{
-			gBoxStatistics.Visible = false;
-		}
+            // TODO: User should be able to allow the limit
+            if (time.Ticks == timeLimit.Ticks)
+                simulator.TimeLimitReached();
+        }
 
-		private void btnRerunSimulation_Click(object sender, EventArgs e)
-		{
-			ReStart();
-			btnTerminate.Visible = false;
-			// run the simulation
-			picBoxPlayPause_Click(null, null);
-		}
+        private void picBoxPlayPause_Click(object sender, EventArgs e)
+        {
+            if (!picBoxPlayPause.Enabled)
+                return;
 
-		private void lbHistory_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			if (this.lbHistory.SelectedItem != null)
-			{
-				History grid = (History)this.lbHistory.SelectedItem;
-				// this.simulator = new GridController(grid.Grid);
-				VisualizeSimulation();
-				// Re adjust the Statistic Data
-				GetStats();
-			}
-		}
+            if (!running)
+            {
+                hasRun = true;
 
-		private void btnTerminate_Click(object sender, EventArgs e)
-		{
-			DialogResult dialogResult = MessageBox.Show("Are you sure you want to terminate the simulation?", "Terminate Simulation", MessageBoxButtons.YesNo);
-			if (dialogResult == DialogResult.Yes)
-			{
-				ReStart();
-				btnTerminate.Visible = false;
-			}
-		}
+                try
+                {
+                    animationLoopTimer.Start();
+                    running = true;
+                    picBoxPlayPause.Image = Icons.Pause;
+                    toolTipPlay.SetToolTip(picBoxPlayPause, "Pause (Spacebar)");
+                    this.simulator.AddToHistory("Simulation started");
+                    UpdateHistory();
+                    this.lbHistory.Enabled = false;
+                    btnTerminate.Visible = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                animationLoopTimer.Stop();
+                running = false;
+                picBoxPlayPause.Image = Icons.Play;
+                toolTipPlay.SetToolTip(picBoxPlayPause, "Resume (Spacebar)");
+                this.simulator.AddToHistory("Simulation paused");
+                UpdateHistory();
+                this.lbHistory.Enabled = true;
+                btnTerminate.Visible = true;
+            }
+        }
 
-		private void btnCalculatePaths_Click(object sender, EventArgs e)
-		{
-			if (running)
-				return;
+        private void btnCloseStatistics_Click(object sender, EventArgs e)
+        {
+            gBoxStatistics.Visible = false;
+        }
 
-			var dialog = new ProgressDialog();
-			var cancelMethod = simulator.SetupInDifferentThread(cancelled =>
-			{
-				if (!cancelled)
-					this.picBoxPlayPause.Enabled = true;
+        private void btnRerunSimulation_Click(object sender, EventArgs e)
+        {
+            ReStart();
+            btnTerminate.Visible = false;
+            // run the simulation
+            picBoxPlayPause_Click(null, null);
+        }
 
-				VisualizeSimulation();
+        private void lbHistory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.lbHistory.SelectedItem != null)
+            {
+                History grid = (History)this.lbHistory.SelectedItem;
+                // this.simulator = new GridController(grid.Grid);
+                VisualizeSimulation();
+                // Re adjust the Statistic Data
+                GetStats();
+            }
+        }
 
-				dialog.Close();
-				dialog.Dispose();
-			}, dialog.SetPercentage, dialog.SetProgressReport);
+        private void btnTerminate_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to terminate the simulation?", "Terminate Simulation", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                ReStart();
+                btnTerminate.Visible = false;
+            }
+        }
 
-			dialog.Cancelled += (object s, EventArgs a) => cancelMethod();
-			dialog.ShowDialog();
-		}
+        private void btnCalculatePaths_Click(object sender, EventArgs e)
+        {
+            if (running)
+                return;
 
-		private void trackBarSpeed_Scroll(object sender, EventArgs e)
-		{
-			lblSpeed.Text = trackBarSpeed.Value.ToString();
-			animationLoopTimer.Interval = 10 + (100 - trackBarSpeed.Value);
-		}
-		#endregion EventHandlers
-	}
+            var dialog = new ProgressDialog();
+            var cancelMethod = simulator.SetupInDifferentThread(cancelled =>
+            {
+                if (!cancelled)
+                    this.picBoxPlayPause.Enabled = true;
+
+                VisualizeSimulation();
+
+                dialog.Close();
+                dialog.Dispose();
+            }, dialog.SetPercentage, dialog.SetProgressReport);
+
+            dialog.Cancelled += (object s, EventArgs a) => cancelMethod();
+            dialog.ShowDialog();
+        }
+
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+            lblSpeed.Text = trackBarSpeed.Value.ToString();
+            animationLoopTimer.Interval = 10 + (100 - trackBarSpeed.Value);
+        }
+
+        private void btEditSessionSettings_Click(object sender, EventArgs e)
+        {
+            var settings = new SessionSettingsForm(this);
+
+            settings.ShowDialog();
+        }
+
+        #endregion EventHandlers
+    }
 }
---
+++ Code/FireSimulation/GUI/SessionSettingsForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FireSimulator.GUI
+{
+    partial class SessionSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTimeLimit = new System.Windows.Forms.Label();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.tbTimeLimitSeconds = new System.Windows.Forms.TextBox();
+            this.tbTimeLimitMinutes = new System.Windows.Forms.TextBox();
+            this.tbTimeLimitHours = new System.Windows.Forms.TextBox();
+            this.lbTimeSpacer = new System.Windows.Forms.Label();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTimeLimit
+            // 
+            this.lbTimeLimit.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbTimeLimit.AutoSize = true;
+            this.lbTimeLimit.Location = new System.Drawing.Point(105, 62);
+            this.lbTimeLimit.Name = "lbTimeLimit";
+            this.lbTimeLimit.Size = new System.Drawing.Size(54, 13);
+            this.lbTimeLimit.TabIndex = 0;
+            this.lbTimeLimit.Text = "Time Limit";
+            // 
+            // btSave
+            // 
+            this.btSave.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btSave.Location = new System.Drawing.Point(14, 132);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(228, 33);
+            this.btSave.TabIndex = 1;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btCancel.Location = new System.Drawing.Point(248, 132);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(228, 33);
+            this.btCancel.TabIndex = 2;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // tbTimeLimitSeconds
+            // 
+            this.tbTimeLimitSeconds.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbTimeLimitSeconds.Location = new System.Drawing.Point(393, 59);
+            this.tbTimeLimitSeconds.Name = "tbTimeLimitSeconds";
+            this.tbTimeLimitSeconds.Size = new System.Drawing.Size(25, 20);
+            this.tbTimeLimitSeconds.TabIndex = 4;
+            // 
+            // tbTimeLimitMinutes
+            // 
+            this.tbTimeLimitMinutes.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbTimeLimitMinutes.Location = new System.Drawing.Point(347, 59);
+            this.tbTimeLimitMinutes.Name = "tbTimeLimitMinutes";
+            this.tbTimeLimitMinutes.Size = new System.Drawing.Size(25, 20);
+            this.tbTimeLimitMinutes.TabIndex = 5;
+            // 
+            // tbTimeLimitHours
+            // 
+            this.tbTimeLimitHours.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbTimeLimitHours.Location = new System.Drawing.Point(300, 59);
+            this.tbTimeLimitHours.Name = "tbTimeLimitHours";
+            this.tbTimeLimitHours.Size = new System.Drawing.Size(25, 20);
+            this.tbTimeLimitHours.TabIndex = 6;
+            // 
+            // lbTimeSpacer
+            // 
+            this.lbTimeSpacer.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbTimeSpacer.AutoSize = true;
+            this.lbTimeSpacer.BackColor = System.Drawing.Color.Transparent;
+            this.lbTimeSpacer.Location = new System.Drawing.Point(331, 62);
+            this.lbTimeSpacer.Name = "lbTimeSpacer";
+            this.lbTimeSpacer.Size = new System.Drawing.Size(10, 13);
+            this.lbTimeSpacer.TabIndex = 7;
+            this.lbTimeSpacer.Text = ":";
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(156, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(193, 29);
+            this.lbTitle.TabIndex = 9;
+            this.lbTitle.Text = "Session Settings";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(378, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = ":";
+            // 
+            // SessionSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 177);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.lbTimeSpacer);
+            this.Controls.Add(this.tbTimeLimitHours);
+            this.Controls.Add(this.tbTimeLimitMinutes);
+            this.Controls.Add(this.tbTimeLimitSeconds);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.lbTimeLimit);
+            this.Name = "SessionSettingsForm";
+            this.Text = "Session Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTimeLimit;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.TextBox tbTimeLimitSeconds;
+        private System.Windows.Forms.TextBox tbTimeLimitMinutes;
+        private System.Windows.Forms.TextBox tbTimeLimitHours;
+        private System.Windows.Forms.Label lbTimeSpacer;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label label2;
+    }
+}
