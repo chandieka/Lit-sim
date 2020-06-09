21ba7b128c0e9784aac1981854d62d740f339aa5
chandieka <candiekatasbihyantra2@gmail.com>
2020-06-09 10:18:50 +0200

    Fix Wording

--- Code/FireSimulation/GUI/Statistics.Designer.cs
+++ Code/FireSimulation/GUI/Statistics.Designer.cs
@@ -38,6 +38,8 @@
             this.lbSearch = new System.Windows.Forms.Label();
             this.pbSelectedPreview = new System.Windows.Forms.PictureBox();
             this.gbStatisticsMenu = new System.Windows.Forms.GroupBox();
+            this.lbl_ratio = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.btn_open_graphs = new System.Windows.Forms.Button();
             this.lbl_name = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
@@ -59,8 +61,6 @@
             this.gbOrderMenu = new System.Windows.Forms.GroupBox();
             this.cbbPreviewOrder = new System.Windows.Forms.ComboBox();
             this.lbSort = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.lbl_ratio = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.gbSearchMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btSearch)).BeginInit();
@@ -215,6 +215,25 @@
             this.gbStatisticsMenu.TabIndex = 1;
             this.gbStatisticsMenu.TabStop = false;
             // 
+            // lbl_ratio
+            // 
+            this.lbl_ratio.AutoSize = true;
+            this.lbl_ratio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ratio.Location = new System.Drawing.Point(148, 287);
+            this.lbl_ratio.Name = "lbl_ratio";
+            this.lbl_ratio.Size = new System.Drawing.Size(0, 16);
+            this.lbl_ratio.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(32, 287);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Success Ratio:";
+            // 
             // btn_open_graphs
             // 
             this.btn_open_graphs.Enabled = false;
@@ -357,9 +376,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(32, 104);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.Size = new System.Drawing.Size(107, 16);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Simulations Ran:";
+            this.label1.Text = "Simulations Run:";
             // 
             // lbStatistics
             // 
@@ -427,25 +446,6 @@
             this.lbSort.Size = new System.Drawing.Size(43, 24);
             this.lbSort.TabIndex = 4;
             this.lbSort.Text = "Sort";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(32, 287);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(105, 16);
-            this.label8.TabIndex = 16;
-            this.label8.Text = "Success Ration:";
-            // 
-            // lbl_ratio
-            // 
-            this.lbl_ratio.AutoSize = true;
-            this.lbl_ratio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_ratio.Location = new System.Drawing.Point(148, 287);
-            this.lbl_ratio.Name = "lbl_ratio";
-            this.lbl_ratio.Size = new System.Drawing.Size(0, 16);
-            this.lbl_ratio.TabIndex = 17;
             // 
             // Statistics
             // 
