50eaf69e7db0c75c70656de628a5d8437d091bc4
Siyana <siyana.stoyanov@gmail.com>
2020-06-18 20:39:42 +0200

    Fixed size of Graph form

--- Code/FireSimulation/GUI/GraphForm.Designer.cs
+++ Code/FireSimulation/GUI/GraphForm.Designer.cs
@@ -42,10 +42,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cartesianChart1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.cartesianChart1.Location = new System.Drawing.Point(12, 16);
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 41);
             this.cartesianChart1.MinimumSize = new System.Drawing.Size(874, 146);
             this.cartesianChart1.Name = "cartesianChart1";
-            this.cartesianChart1.Size = new System.Drawing.Size(874, 146);
+            this.cartesianChart1.Size = new System.Drawing.Size(915, 301);
             this.cartesianChart1.TabIndex = 0;
             this.cartesianChart1.Text = "cartesianChart1";
             // 
@@ -53,7 +53,7 @@
             // 
             this.pieChart1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.pieChart1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.pieChart1.Location = new System.Drawing.Point(12, 172);
+            this.pieChart1.Location = new System.Drawing.Point(12, 380);
             this.pieChart1.Name = "pieChart1";
             this.pieChart1.Size = new System.Drawing.Size(391, 440);
             this.pieChart1.TabIndex = 1;
@@ -64,9 +64,9 @@
             this.cartesianChart2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cartesianChart2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.cartesianChart2.Location = new System.Drawing.Point(409, 172);
+            this.cartesianChart2.Location = new System.Drawing.Point(427, 380);
             this.cartesianChart2.Name = "cartesianChart2";
-            this.cartesianChart2.Size = new System.Drawing.Size(477, 217);
+            this.cartesianChart2.Size = new System.Drawing.Size(500, 217);
             this.cartesianChart2.TabIndex = 2;
             this.cartesianChart2.Text = "Numer of Deaths";
             // 
@@ -75,9 +75,9 @@
             this.cartesianChart3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cartesianChart3.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.cartesianChart3.Location = new System.Drawing.Point(409, 395);
+            this.cartesianChart3.Location = new System.Drawing.Point(427, 603);
             this.cartesianChart3.Name = "cartesianChart3";
-            this.cartesianChart3.Size = new System.Drawing.Size(477, 217);
+            this.cartesianChart3.Size = new System.Drawing.Size(500, 217);
             this.cartesianChart3.TabIndex = 3;
             this.cartesianChart3.Text = "cartesianChart3";
             // 
@@ -86,9 +86,9 @@
             this.cartesianDeathProgression.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cartesianDeathProgression.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.cartesianDeathProgression.Location = new System.Drawing.Point(12, 618);
+            this.cartesianDeathProgression.Location = new System.Drawing.Point(12, 850);
             this.cartesianDeathProgression.Name = "cartesianDeathProgression";
-            this.cartesianDeathProgression.Size = new System.Drawing.Size(874, 217);
+            this.cartesianDeathProgression.Size = new System.Drawing.Size(915, 238);
             this.cartesianDeathProgression.TabIndex = 4;
             this.cartesianDeathProgression.Text = "Death Progression";
             // 
@@ -96,16 +96,16 @@
             // 
             this.vScrollBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.vScrollBar1.Location = new System.Drawing.Point(894, 16);
+            this.vScrollBar1.Location = new System.Drawing.Point(935, 6);
             this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(17, 320);
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 521);
             this.vScrollBar1.TabIndex = 5;
             // 
             // GraphForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(920, 345);
+            this.ClientSize = new System.Drawing.Size(961, 546);
             this.Controls.Add(this.vScrollBar1);
             this.Controls.Add(this.cartesianDeathProgression);
             this.Controls.Add(this.cartesianChart3);
